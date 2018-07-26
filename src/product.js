c2d57cb25602b3a9fe057de19e5bd782d0e8dee0
sh1vero <grzesiekdziubek@gmail.com>
2018-07-26 23:31:55 +0200

    updated dependencies to latest version

--- src/product.js
+++ src/product.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import productService from './products-service';
 
 class Products extends Component {
+
     render() {
 
         const InStock = <div className="card-text text-success">In stock</div>;
         const notAvailable = <div className="card-text text-danger">Not available</div>;
         const product = productService.map(
             (product, index) =>
-
                 <div key={index} className="card mb-3">
                     <div className="card-body">
                         <div className="card-title">
                             <h6>{product.name}</h6>
+
                             {product.instock ? InStock : notAvailable}
                             <div className="card-text">{product.price}</div>
                             <a className="btn btn-primary float-right" href="#" role="button">Add to cart</a>
